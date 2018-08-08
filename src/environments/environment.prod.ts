9aabb0f34bec9f11558c44e9b358b3e165a0a6aa
robby <ta7382@gmail.com>
2018-08-08 20:43:33 +0800

    Add firebase env to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDETAGB05xiUWrie-MSDFKVqmfuKDw3-Sg",
+    authDomain: "bikini-bottom-racing.firebaseapp.com",
+    databaseURL: "https://bikini-bottom-racing.firebaseio.com",
+    projectId: "bikini-bottom-racing",
+    storageBucket: "bikini-bottom-racing.appspot.com",
+  }
 };
